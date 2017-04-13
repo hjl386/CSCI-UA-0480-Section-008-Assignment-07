1ab787fd5de2264f06e74b0153a19bfca62e066f
Han Jie Lee <hjl386@nyu.edu>
2017-04-13 09:13:04 -0400

    Initial Setup

---
+++ README.md
@@ -0,0 +1,14 @@
+#BLACK JACKS Homework 7
+
+## Personal Information 
+
+Han Jie Lee
+hjl386
+
+## Description
+
+Implementation of a black jacks game
+
+## Extra Credit
+Did not do YET 
+
--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // app.js for homework #7 - blackjack
+
+const express = require('express');
+const path = require('path');
+
+const [PORT, HOST] = [3000, '127.0.0.1'];
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, HOST);
