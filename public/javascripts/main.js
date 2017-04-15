60ce8cf7d91bf730f950937e005d4b0f60d163ac
Han Jie Lee <hjl386@nyu.edu>
2017-04-15 12:16:36 -0400

    Fixed deck creation bug

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,8 @@
 //Main.js 
 function randomizer(array){
 	const ran = Math.floor(Math.random() * array.length);	
-	const val = array[ran];
+	const val = array[ran];	
+//	console.log("RAN", val);
 	return val;
 }
 
@@ -23,6 +24,7 @@ function createLeftOverDeck(arrSuit, arrFace, deck){
 	
 function elt(type){
 	var ele = document.createElement(type);
+	//ele.className = 'card';
 	for (var i = 1; i < arguments.length; i++){
 		var child = arguments[i];
 		if(typeof child === 'string'){
@@ -46,9 +48,8 @@ function calculateScore(hand){
 			return acc += 1;
 		} else if(cur.face === 'J' || cur.face === 'Q' || cur.face === 'K'){
 			return acc += 10;
-		}
-		else{
-			return acc += cur.face;
+		} else{
+			return acc += parseInt(cur.face);
 		}
 	},0);
 	return score;
@@ -59,23 +60,49 @@ function clickHandler(evt){
 	const startClass = document.querySelector('.start');
 	startClass.classList.toggle('hideForm');
 	const values = document.querySelector('#startValues').value;
-	const inputVal = values.split(",");
+	let inputVal = values.split(",");
 	const suits = ['spade', 'club', 'heart', 'diamond'];
 	const faces = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
 	const deck = [];
-	if(inputVal[0] !== ''){
-		while(deck.length !== inputVal.length){
+	if(inputVal[0] === ''){
+		inputVal = [];
+	} /*
+	inputVal.forEach(val => {
+		let check = true;
+		while(check){
 			const ran = randomizer(suits);
-			const tempObject = {'suits' : ran, 'face' : inputVal[deck.length]};
+			const tempObj = {'suits': ran, 'face': val};
 			const tempArr = deck.filter(ele => {
-				return (ele['suits'] === tempObject['suits'] && ele['face'] === tempObject['face']);
+				return (ele.suits === ran && ele.face === val);
 			});
 			if(tempArr.length === 0){
-				deck.push(tempObject);
+				deck.push(tempObj);
+				check = false;
+			} else{
+				check = true;
 			}
+		}	
+	}); */
+	while(deck.length !== inputVal.length){
+		const ran = randomizer(suits);
+		let val;
+		if(inputVal[deck.length] === 'A' || inputVal[deck.length] === 'K' || inputVal[deck.length] === 'Q' || inputVal[deck.length] === 'J'){
+			val = inputVal[deck.length];
+		}else if(inputVal[deck.length] !== 'A' || inputVal[deck.length] !== 'K' || inputVal[deck.length] !== 'Q' || inputVal[deck.length] !== 'J'){
+			val = parseInt(inputVal[deck.length]);
+		} 
+		const tempObj = {'suits': ran, 'face':val};
+		const tempArr = deck.filter(ele => {
+			return (ele.suits === tempObj.suits && ele.face === tempObj.face);
+		});
+		if(tempArr.length === 0){
+			deck.push(tempObj);
 		}
 	}
 	const deckFull = createLeftOverDeck(suits, faces, deck);
+	deckFull.forEach(ele => {
+		console.log(ele);
+	});
 	const compHand = [];
 	const myHand = [];
 	for(let i = 0; i < 4; i++){
@@ -87,11 +114,14 @@ function clickHandler(evt){
 	}
 	const cpScore = calculateScore(compHand);
 	const myScore = calculateScore(myHand);
-	const cpTotal = elt('div', elt('p', 'Computer Score', cpScore.toString()));
+	const cpTotal = elt('div', elt('p', 'Computer Hand - Total: ', cpScore.toString()));
 	document.querySelector('.game').appendChild(cpTotal); 	
 	createAndAppendHand('.game', 'div', 'p', compHand);
+	const myTotal = elt('div', elt('p', 'Player Hand - Total: ', myScore.toString()));
+	document.querySelector('.game').appendChild(myTotal); 	
 	createAndAppendHand('.game', 'div', 'p', myHand);
-			
+	const game = document.querySelector('.game');
+//	game.classList.toggle('card');
 }
 
 function main(){
